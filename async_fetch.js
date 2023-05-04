7e770e6a4e63d3316ad095c75f0bf14b50abc912
Rajan Kumar Singh <rajan250901@gmail.com>
2023-05-05 00:54:52 +0530

    files updated

---
+++ async_fetch.js
@@ -0,0 +1,64 @@
+const redux=require('redux');
+const { createStore, applyMiddleware } = redux;
+const thunkMiddleware=require('redux-thunk').default;
+const axios=require('axios');
+const initialState={
+    loading:false,
+    users:[],
+    error:''
+}
+
+const FETCH_USERS_REQUEST="FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS="FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE="FETCH_USERS_FAILURE";
+
+function fetchusersrequest(){
+    return{
+        type:FETCH_USERS_REQUEST,
+        payload:false,
+        error:''
+    }
+}
+
+function fetchuserssuccess(users){
+    return{
+        type:FETCH_USERS_SUCCESS,
+        payload:users,
+        error:''
+    }
+}
+
+function fetchusersfailure(error){
+    return{
+        type:FETCH_USERS_FAILURE,
+        payload:false,
+        error:error
+    }
+}
+
+const reducer=(state=initialState,action)=>{
+    switch(action.type){
+        case FETCH_USERS_REQUEST: return{...state,loading:true}
+        case FETCH_USERS_SUCCESS: return{loading:false,users:action.payload,error:''}
+        case FETCH_USERS_FAILURE: return{loading:false,users:[],error:action.payload}
+        default: return state.num
+    }
+}
+
+const fetchusers = ()=>{
+    return function(dispatch){
+        dispatch(fetchusersrequest())
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response=>{
+            const users=response.data.map(user=>user.email)
+            dispatch(fetchuserssuccess(users))
+        })
+        .catch(error=>{
+            dispatch(fetchusersfailure(error.message))
+        })
+    }
+}
+
+const store = createStore(reducer,applyMiddleware(thunkMiddleware))
+store.subscribe(()=>{console.log(store.getState())})
+store.dispatch(fetchusers())
